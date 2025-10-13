33021f894ea069d7fc139104d3c741e1bb392ecd
ak47andrew <ak47reborntumpa@gmail.com>
2025-10-13 20:19:55 +0300

    Change code structure because python can eat my entire ass

--- generate_diff.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 65001 > nul
-git add .
-git diff --staged > .diff
--- src/visualizations/translation_process.py
+++ src/visualizations/translation_process.py
@@ -1,13 +1,14 @@
+import manim
 import pathlib
 from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 2:
-        print(f"Usage: python {argv[0]} <NJ file>")
+        print(f"Usage: python {argv[0]} <Bee file>")
         exit(1)
 
     try:
         data = pathlib.Path(argv[1]).read_text()
     except Exception as e:
-        print(f"Usage: python {argv[0]} <NJ file>\n-----\nExcpetion occured: {e}")
+        print(f"Usage: python {argv[0]} <Bee file>\n-----\nExcpetion occured: {e}")
         exit(1)
